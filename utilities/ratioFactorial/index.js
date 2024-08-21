a1cad71ec4879337c05a5b04eaa2df869f022eb9
saikirangumpu <saikirangumpusps@gmail.com>
2024-08-21 06:51:27 +0000

    solution

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,14 @@
+let ratioOfTwoNumbers = require('../ratio/index.js')
+let factorialOfNumber = require('../factorial/index.js')
+
+let ratioAndFactorial = (num1, num2, num3) => {
+  let ratio = ratioOfTwoNumbers(num1, num2)
+  let factorial = factorialOfNumber(num3)
+
+  return {
+    ratio,
+    factorial,
+  }
+}
+
+module.exports = ratioAndFactorial
